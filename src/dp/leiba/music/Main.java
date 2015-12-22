70b6c6c5ff621eb2d364eb4c9c1cbd9028972186
leiba <voloshin@segater.com>
2015-12-22 14:25:47 +0200

    master // init project

---
+++ src/dp/leiba/music/Main.java
@@ -0,0 +1,18 @@
+package dp.leiba.music;
+
+/**
+ * Main.
+ */
+public class Main
+{
+
+    /**
+     * Start point.
+     *
+     * @param args Input arguments.
+     */
+    public static void main(String[] args)
+    {
+
+    }
+}
