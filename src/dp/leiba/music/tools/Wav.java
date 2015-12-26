ff1de585a04e5a62333ab968c6196e8f876eb136
leiba <voloshin@segater.com>
2015-12-26 14:35:36 +0200

    master // generate wav file

--- src/dp/leiba/music/tools/Wav.java
+++ src/dp/leiba/music/tools/Wav.java
@@ -1,6 +1,9 @@
 package dp.leiba.music.tools;
 
+import com.sun.deploy.util.ArrayUtil;
+
 import java.io.*;
+import java.lang.reflect.Array;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.file.Files;
@@ -14,145 +17,39 @@ import java.util.Random;
  */
 public class Wav
 {
-    public static final int I_CHANNEL_MONO     = 1;
-    public static final int I_CHANNEL_STEREO   = 2;
-
     protected String  _iChunkId       = "RIFF";   // 4b
-    protected int     _iChunkSize     = 0;        // 4b
+    protected int     _iChunkSize     = 58;       // 4b
     protected String  _iFormat        = "WAVE";   // 4b
     protected String  _iSubChunk1Id   = "fmt ";   // 4b
     protected int     _iSubChunk1Size = 16;       // 4b
-    protected short   _iAudioFormat   = 1;        // 2b
+    protected short   _iAudioFormat   = 7;        // 2b
     protected short   _iNumChannels   = 1;        // 2b 1/2
     protected int     _iSampleRate    = 8000;     // 4b 8000/44100
-    protected int     _iByteRate      = 0;        // 4b Bytes per second
-    protected short   _iBlockAlign    = 0;        // 2b Bytes in sample for all channels
-    protected short   _iBitsPerSample = 0;        // 2b Bits in sample
+    protected int     _iByteRate      = 8000;     // 4b Bytes per second
+    protected short   _iBlockAlign    = 1;        // 2b Bytes in sample for all channels
+    protected short   _iBitsPerSample = 8;        // 2b Bits in sample
     protected String  _iSubChunk2Id   = "data";   // 4b
-    protected int     _iSubChunk2Size = 16;       // 4b Bytes in data part
+    protected int     _iSubChunk2Size = 3;        // 4b Bytes in data part
 
-    protected ArrayList<byte[]> _iFramesLeft  = new ArrayList<byte[]>();
-    protected ArrayList<byte[]> _iFramesRight = new ArrayList<byte[]>();
+    protected ArrayList<byte[]> _iFrames  = new ArrayList<byte[]>();
 
     /**
      * Wav.
      */
     public Wav()
     {
-
-    }
-
-    /**
-     * Wav.
-     *
-     * @param path File path.
-     */
-    public Wav(String path)
-    {
-        try {
-            byte[]  data = Files.readAllBytes(Paths.get(path));
-            byte[]  sample;
-            int     sampleHalf;
-
-            _iChunkId       = _byteToString(data, 0, 4);
-            _iChunkSize     = _byteToInt(data, 4, 8);
-            _iFormat        = _byteToString(data, 8, 12);
-            _iSubChunk1Id   = _byteToString(data, 12, 16);
-            _iSubChunk1Size = _byteToInt(data, 16, 20);
-            _iAudioFormat   = _byteToShort(data, 20, 22);
-            _iNumChannels   = _byteToShort(data, 22, 24);
-            _iSampleRate    = _byteToInt(data, 24, 28);
-            _iByteRate      = _byteToInt(data, 28, 32);
-            _iBlockAlign    = _byteToShort(data, 32, 34);
-            _iBitsPerSample = _byteToShort(data, 34, 36);
-            _iSubChunk2Id   = _byteToString(data, 36, 40);
-            _iSubChunk2Size = _byteToInt(data, 40, 44);
-
-            for (int i = 44; i < data.length - _iBlockAlign; i+= _iBlockAlign) {
-                sample      = Arrays.copyOfRange(data, i, i + _iBlockAlign);
-                sampleHalf  = sample.length / 2;
-
-                if (_iNumChannels == I_CHANNEL_MONO) {
-                    _iFramesLeft.add(sample);
-                } else {
-                    _iFramesLeft.add(Arrays.copyOfRange(sample, 0, sampleHalf));
-                    _iFramesRight.add(Arrays.copyOfRange(sample, sampleHalf, sampleHalf));
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Get channel frames.
-     *
-     * @param channel Channel.
-     *
-     * @return Frames.
-     */
-    public ArrayList<byte[]> getFrames(int channel) {
-        return channel == I_CHANNEL_MONO ? _iFramesLeft : _iFramesRight;
-    }
-
-    /**
-     * Get channel amplify.
-     *
-     * @param channel Channel number.
-     *
-     * @return Amplify
-     */
-    public int[] getAmplify(int channel)
-    {
-        ArrayList<byte[]>   frames  = getFrames(channel);
-        int[]               amplify = new int[frames.size()];
-        byte[]              frame;
-
-        for (int i = 0; i < frames.size(); i++) {
-            frame = frames.get(i);
-            amplify[i] = frame.length == 2 ? _byteToShort(frame, 0, frame.length)
-                    : _byteToInt(frame, 0, frame.length);
-        }
-
-        return amplify;
+        // Empty wave file.
     }
 
     /**
      * Set frames for channel.
      *
-     * @param frames  Frames.
-     * @param channel Channel.
+     * @param frames Frames.
      */
-    public void setFrames(ArrayList<byte[]> frames, int channel)
+    public void setFrames(ArrayList<byte[]> frames)
     {
-        if (channel == I_CHANNEL_MONO) {
-            _iFramesLeft.clear();
-            _iFramesLeft.addAll(frames);
-        } else {
-            _iFramesRight.clear();
-            _iFramesRight.addAll(frames);
-        }
-    }
-
-    /**
-     * Fill random amplify.
-     *
-     * @param size Number of frames.
-     */
-    public void fillRandom(int size)
-    {
-        ArrayList<byte[]>   frames = new ArrayList<byte[]>();
-        Random              random = new Random();
-
-        for (int i = 0; i < size; i++) {
-            frames.add(ByteBuffer.allocate(2).putShort((short) random.nextInt(250)).array());
-        }
-
-        setFrames(frames, I_CHANNEL_MONO);
-
-        if (_iNumChannels == I_CHANNEL_STEREO) {
-            setFrames(frames, I_CHANNEL_STEREO);
-        }
+        _iFrames.clear();
+        _iFrames.addAll(frames);
     }
 
     /**
@@ -178,14 +75,10 @@ public class Wav
             data = _byteAppend(data, _byteFromShort(_iBlockAlign));
             data = _byteAppend(data, _byteFromShort(_iBitsPerSample));
             data = _byteAppend(data, _iSubChunk2Id.getBytes());
-            data = _byteAppend(data, _byteFromInt(_iSubChunk2Size));
+            data = _byteAppend(data, _byteFromInt(_iFrames.size())); // _byteFromInt(_iSubChunk2Size));
 
-            for (int i = 0; i < _iFramesLeft.size(); i++) {
-                data = _byteAppend(data, _iFramesLeft.get(i));
-
-                if (_iNumChannels == I_CHANNEL_STEREO) {
-                    data = _byteAppend(data, _iFramesRight.get(i));
-                }
+            for (int i = 0; i < _iFrames.size(); i++) {
+                data = _byteAppend(data, _iFrames.get(i));
             }
 
             stream.write(data);
@@ -193,44 +86,6 @@ public class Wav
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    /**
-     * Convert byte array to string.
-     *
-     * @param data Byte data.
-     *
-     * @return String.
-     */
-    private String _byteToString(byte[] data, int from, int to)
-    {
-        return new String(Arrays.copyOfRange(data, from, to));
-    }
-
-    /**
-     * Convert byte array to short.
-     *
-     * @param data Byte data.
-     *
-     * @return Short.
-     */
-    private short _byteToShort(byte[] data, int from, int to)
-    {
-        return ByteBuffer.wrap(Arrays.copyOfRange(data, from, to))
-                .order(ByteOrder.LITTLE_ENDIAN).getShort();
-    }
-
-    /**
-     * Convert byte array to int.
-     *
-     * @param data Byte data.
-     *
-     * @return Int.
-     */
-    private int _byteToInt(byte[] data, int from, int to)
-    {
-        return ByteBuffer.wrap(Arrays.copyOfRange(data, from, to))
-                .order(ByteOrder.LITTLE_ENDIAN).getInt();
     }
 
     /**
@@ -273,9 +128,10 @@ public class Wav
      */
     private byte[] _byteAppend(byte[] data1, byte[] data2)
     {
-        byte[] total = new byte[data1.length + data2.length];
+        int i           = 0;
+        byte[] total    = new byte[data1.length + data2.length];
 
-        for (int i = 0; i < total.length; i++) {
+        for (; i < total.length; i++) {
             total[i] = i < data1.length ? data1[i] : data2[i - data1.length];
         }
 
@@ -288,12 +144,12 @@ public class Wav
      * @param data Array.
      */
     private void _byteReverse(byte[] data) {
-        int left = 0;
-        int right = data.length - 1;
+        int left    = 0;
+        int right   = data.length - 1;
 
-        while( left < right ) {
-            byte temp = data[left];
-            data[left] = data[right];
+        while(left < right) {
+            byte temp   = data[left];
+            data[left]  = data[right];
             data[right] = temp;
 
             left++;
