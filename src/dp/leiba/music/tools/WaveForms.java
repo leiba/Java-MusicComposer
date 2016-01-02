30e064cb0d8f3f0da2e32437b8ac3a22add00c1f
leiba <leibaWorld@gmail.com>
2016-01-03 01:21:24 +0300

    master // rhythm bass

--- src/dp/leiba/music/tools/WaveForms.java
+++ src/dp/leiba/music/tools/WaveForms.java
@@ -114,46 +114,4 @@ public class WaveForms
 
         return wave;
     }
-    
-    /**
-     * Generate kick.
-     * 
-     * @param amplitude Amplitude.
-     * 
-     * @return Wave.
-     */
-    public static double[] kick(double amplitude)
-    {
-        double[] wave = new double[0];
-        
-        return wave;
-    }
-    
-    /**
-     * Generate snare.
-     * 
-     * @param amplitude Amplitude.
-     * 
-     * @return Wave.
-     */
-    public static double[] snare(double amplitude)
-    {
-        double[] wave = new double[0];
-        
-        return wave;
-    }
-    
-    /**
-     * Generate hat.
-     * 
-     * @param amplitude Amplitude.
-     * 
-     * @return Wave.
-     */
-    public static double[] hat(double amplitude)
-    {
-        double[] wave = new double[0];
-        
-        return wave;
-    }
 }
---
+++ src/dp/leiba/music/tools/WaveInstruments.java
@@ -0,0 +1,47 @@
+package dp.leiba.music.tools;
+
+public class WaveInstruments
+{
+    
+    /**
+     * Generate kick.
+     * 
+     * @param amplitude Amplitude.
+     * 
+     * @return Wave.
+     */
+    public static double[] kick(double amplitude)
+    {
+        double[] wave = new double[0];
+        
+        return wave;
+    }
+    
+    /**
+     * Generate snare.
+     * 
+     * @param amplitude Amplitude.
+     * 
+     * @return Wave.
+     */
+    public static double[] snare(double amplitude)
+    {
+        double[] wave = new double[0];
+        
+        return wave;
+    }
+    
+    /**
+     * Generate hat.
+     * 
+     * @param amplitude Amplitude.
+     * 
+     * @return Wave.
+     */
+    public static double[] hat(double amplitude)
+    {
+        double[] wave = new double[0];
+        
+        return wave;
+    }
+}
