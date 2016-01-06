54decbf95e3fe27e280fc7343d9c14291db6d8f6
leiba <voloshin@segater.com>
2016-01-06 09:21:42 +0200

    master // composer wave

--- src/dp/leiba/music/creation/Composer.java
+++ src/dp/leiba/music/creation/Composer.java
@@ -1,26 +1,35 @@
 package dp.leiba.music.creation;
 
+import dp.leiba.music.tools.Wav;
+
 /**
  * Composer.
  */
 public class Composer
 {
-    private int     _cBars  = 4;
-    private int     _cBeats = 4;
+    private Wav         _cWav   = new Wav();
+    private int         _cBarSize;
+    private double[]    _cWave;
 
-    private int[]   _cRhythm;
-    private int     _cNote;
-    private boolean _cIsMajor;
-    private int[][] _cChords;
-    private int[]   _cNotes;
+    private int         _cBars  = 4;
+    private int         _cBeats = 4;
 
-    private int[]   _cLead;
-    private int[][] _cPluck;
-    private int[]   _cBass;
-    private int[][] _cDrums;
+    private int[]       _cRhythm;
+    private int         _cNote;
+    private boolean     _cIsMajor;
+    private int[][]     _cChords;
+    private int[]       _cNotes;
+
+    private int[]       _cLead;
+    private int[][]     _cPluck;
+    private int[]       _cBass;
+    private int[][]     _cDrums;
 
     public Composer()
     {
+        _cBarSize   = _cWav.getBytesPerSecond() / 2;
+        _cWave      = new double[_cBarSize * _cBars];
+
         _cRhythm    = Rhythm.getRhythmMelody(_cBars, _cBeats);
         _cNote      = Melody.getNote();
         _cIsMajor   = Melody.getIsMajor();
@@ -31,6 +40,8 @@ public class Composer
         _cPluck     = Melody.getPluck(_cBars, _cBeats, _cRhythm, _cChords);
         _cBass      = Melody.getBass(_cBars, _cBeats, _cRhythm, _cChords);
         _cDrums     = Rhythm.getRhythmDrums(_cBars, _cBeats);
+
+
     }
 
 }
