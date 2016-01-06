decb7d26f81fed6106350cb67509ab830d71bec0
leiba <voloshin@segater.com>
2016-01-06 09:10:00 +0200

    master // wave effects

---
+++ src/dp/leiba/music/tools/WaveEffect.java
@@ -0,0 +1,24 @@
+package dp.leiba.music.tools;
+
+/**
+ * Wave effect.
+ */
+public class WaveEffect
+{
+
+    /**
+     * Reverberation.
+     */
+    public static void reverberation()
+    {
+
+    }
+
+    /**
+     * Delay.
+     */
+    public static void delay()
+    {
+
+    }
+}
