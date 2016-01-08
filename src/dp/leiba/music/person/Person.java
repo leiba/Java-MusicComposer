e2d964a50c4efdc315bd2b9b7522ce37bfac347c
leiba <voloshin@segater.com>
2016-01-08 11:18:22 +0200

    master // paerson indent

--- src/dp/leiba/music/person/Person.java
+++ src/dp/leiba/music/person/Person.java
@@ -10,96 +10,96 @@ import dp.leiba.music.creation.Theory;
  */
 public class Person
 {
-	public static final String NAME = "Bit B";
-	public static final String REST = "REST";
-	
-	/**
-	 * Say.
-	 * 
-	 * @param what What.
-	 */
-	public static void say(String what)
-	{
-		System.out.println(NAME + ", say: " + what);
-	}
-	
-	/**
-	 * Debug.
-	 * 
-	 * @param name  Name.
-	 * @param value Value.
-	 */
-	public static void debug(String name, String value)
-	{
-		System.out.println(NAME + ", debug (" + name + "): " + value);
-	}
-	
-	/**
-	 * Debug.
-	 * 
-	 * @param name Name.
-	 * @param note Note.
-	 */
-	public static void debug(String name, int note)
-	{
-		debug(name, Theory.getNoteName(note) + "(" + note + ")");
-	}
-	
-	/**
-	 * Debug.
-	 * 
-	 * @param name Name.
-	 * @param is   Is.
-	 */
-	public static void debug(String name, boolean is)
-	{
-		debug(name, is + "");
-	}
-	
-	/**
-	 * Debug.
-	 * 
-	 * @param name  Name.
-	 * @param notes Notes.
-	 */
-	public static void debug(String name, int[] notes)
-	{
-		int i           = 0;
+    public static final String NAME = "Bit B";
+    public static final String REST = "REST";
+
+    /**
+     * Say.
+     *
+     * @param what What.
+     */
+    public static void say(String what)
+    {
+        System.out.println(NAME + ", say: " + what);
+    }
+
+    /**
+     * Debug.
+     *
+     * @param name  Name.
+     * @param value Value.
+     */
+    public static void debug(String name, String value)
+    {
+        System.out.println(NAME + ", debug (" + name + "): " + value);
+    }
+
+    /**
+     * Debug.
+     *
+     * @param name Name.
+     * @param note Note.
+     */
+    public static void debug(String name, int note)
+    {
+        debug(name, Theory.getNoteName(note) + "(" + note + ")");
+    }
+
+    /**
+     * Debug.
+     *
+     * @param name Name.
+     * @param is   Is.
+     */
+    public static void debug(String name, boolean is)
+    {
+        debug(name, is + "");
+    }
+
+    /**
+     * Debug.
+     *
+     * @param name  Name.
+     * @param notes Notes.
+     */
+    public static void debug(String name, int[] notes)
+    {
+        int i           = 0;
         String[] debug  = new String[notes.length];
 
         for (; i < notes.length; i++) {
-        	if (notes[i] == Rhythm.MELODY_REST) {
-        		debug[i] = REST;
-        	} else {
-        		debug[i] = Theory.getNoteName(notes[i]) + "(" + notes[i] + ")";
-        	}        	
+            if (notes[i] == Rhythm.MELODY_REST) {
+                debug[i] = REST;
+            } else {
+                debug[i] = Theory.getNoteName(notes[i]) + "(" + notes[i] + ")";
+            }
         }
 
         debug(name, Arrays.toString(debug));
-	}
-	
-	/**
-	 * Debug.
-	 * 
-	 * @param name  Name.
-	 * @param notes Notes.
-	 */
-	public static void debug(String name, int[][] notes)
-	{
-		int i, j;
-		String[] line;
+    }
+
+    /**
+     * Debug.
+     *
+     * @param name  Name.
+     * @param notes Notes.
+     */
+    public static void debug(String name, int[][] notes)
+    {
+        int i, j;
+        String[] line;
         String[] debug  = new String[notes.length];
 
         for (i = 0; i < notes.length; i++) {
-        	line = new String[notes[i].length];
-        	
-        	for (j = 0; j < notes[i].length; j++) {
-        		line[j] = Theory.getNoteName(notes[i][j]) + "(" + notes[i][j] + ")";
-        	}
-        	
-        	debug[i] = Arrays.toString(line);
+            line = new String[notes[i].length];
+
+            for (j = 0; j < notes[i].length; j++) {
+                line[j] = Theory.getNoteName(notes[i][j]) + "(" + notes[i][j] + ")";
+            }
+
+            debug[i] = Arrays.toString(line);
         }
 
         debug(name, Arrays.toString(debug));
-	}
+    }
 }
