9fa5f35f441e980c17037bea85a660f165c06191
leibaWorld@gmail.com <leibaWorld@gmail.com>
2016-01-10 14:42:39 +0200

    master // eq filters

--- src/dp/leiba/music/tools/WaveFilters.java
+++ src/dp/leiba/music/tools/WaveFilters.java
@@ -2,8 +2,32 @@ package dp.leiba.music.tools;
 
 import java.util.ArrayList;
 
+/**
+ * Wave Filters.
+ * 
+ * Low pass. Пропуск НЧ.
+ * High pass. Пропуск ВЧ.
+ * Band pass. Пропуск полосы.
+ * Notch. Всех кроме полосы.
+ * Peaking\Bell. Колоколообразный.
+ * Low shelf. Полка НЧ.
+ * High shelf. Полка ВЧ.
+ */
 public class WaveFilters
 {
+	
+	/**
+	 * Equalizer.
+	 * 
+	 * @param type Type of filter.
+	 * @param freq Frequency.
+	 * @param q    Q factor. Frequencies width. 
+	 * @param gain Gain frequencies.
+	 */
+	public static void eq(int type, double freq, double q, double gain)
+	{
+		
+	}
 	
 	public static double[] band(double[] signal, int pointsPerSecond, double from, double to)
 	{
